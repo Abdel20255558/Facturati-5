524ac7606c3706e78d82aac97550fcc73d6a3e44
yayayhaya3-lab <yayayhaya3@gmail.com>
2025-09-15 16:30:59 +0100

    Updated HomePage.tsx

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -515,7 +515,7 @@ export default function HomePage() {
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             <div className="md:col-span-2">
               <div className="flex items-center space-x-3 mb-4">
-                <div  className="w-8 h-8 bg-gradient-to-br from-teal-600 to-red-600 rounded-lg flex items-center justify-center shadow-lg">
+                <div  className="w-8 h-8 bg-gradient-to-br from-black-200 to-red-600  rounded-lg flex items-center justify-center shadow-lg">
                   <img 
                     src="https://i.ibb.co/kgVKRM9z/20250915-1327-Conception-Logo-Color-remix-01k56ne0szey2vndspbkzvezyp-1.png" 
                     alt="Facturati Logo" 
