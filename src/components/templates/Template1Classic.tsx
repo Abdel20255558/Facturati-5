a7c81761eeab14c263f6f72cee42a18319e7e6c9
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-16 17:42:29 +0100

    Updated Template1Classic.tsx

--- src/components/templates/Template1Classic.tsx
+++ src/components/templates/Template1Classic.tsx
@@ -18,7 +18,7 @@ export default function Template1Classic({ data, type, includeSignature = false 
       style={{
         fontFamily: 'Arial, sans-serif',
         width: '780px',       // largeur A4
-        minHeight: '900',  // hauteur A4
+        minHeight: '1200',  // hauteur A4
         display: 'flex',
       }}
     >
